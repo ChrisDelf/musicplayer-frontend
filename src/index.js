3ea1df43bf27f6f933468279ebcabd1f4b6aebac
Chris <chrisdel@protonmail.com>
2021-09-28 20:33:17 -0400

    fixed issues with the package.json file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import combineReducers from './reducers/index.js';
+// Import redux persist
+import storage from 'redux-persist/lib/storage';
+import {persistStore, persistReducer} from 'redux-persist';
+//PersistGate wrapped around root component
+import {PersistGate} from 'redux-persist/integration/react';
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+console.log(process.env.AUTH_CLIENT)
+console.log(process.env.REACT_APP_CLIENT)
+//Persist combined reducers
+const persistedReducer = persistReducer(persistConfig, combineReducers)
+//Store now has access to persisted reducers
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+
+const persistor = persistStore(store);
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <App/>
+        </PersistGate>
+    </Provider>,
+    rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
